8d44e34655f79ec3f0d0b4b77bc6b3b129038d91
FliiFe <theophile.cailliau@gmail.com>
2016-12-23 22:29:48 +0100

    Fixed verbose output with -q flag

--- init.fish
+++ init.fish
@@ -1,10 +1,10 @@
 function init -a path --on-event init_powerline
-  type powerline-daemon; and powerline-daemon -q;
+  type -q powerline-daemon; and powerline-daemon -q;
 
   set -q POWERLINE_PACKAGE_DIR; or set -gx POWERLINE_PACKAGE_DIR (pip show powerline-status 2>/dev/null | grep Location | awk '{ print $2 }')
   set fish_function_path $fish_function_path "$POWERLINE_PACKAGE_DIR/powerline/bindings/fish"
 
-  if type powerline-setup
+  if type -q powerline-setup
     powerline-setup
   else
     echo "Please install powerline"
